c4ea9d5dd608ecbf4e3bcc0e062ab641678ff774
Mohammed Ali Kunda <mkund01@jaguar.tamu.edu>
2022-03-31 16:23:20 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mohammed on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
